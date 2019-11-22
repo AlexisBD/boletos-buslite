72bf8bb4e38274e5d4d0ede8703d47830de49606
Milton Hernández <171122@ids.upchiapas.edu.mx>
2019-11-22 00:56:17 -0600

    fecha hora

--- src/app/pages/ventas/resultado-busqueda-boleto/resultado-busqueda-boleto.component.ts
+++ src/app/pages/ventas/resultado-busqueda-boleto/resultado-busqueda-boleto.component.ts
@@ -29,12 +29,12 @@ export class ResultadoBusquedaBoletoComponent implements OnInit {
     this.api.getAutobusById(this.busId).subscribe(
       (data) => {
         console.log(data);
-        this.horario_salida = data["horario_salida"];
+        this.horario_salida = this.horaFecha(data["horario_salida"]);//data["horario_salida"];
         this.lugar_destino = data["lugar_destino"];
         this.tipo_bus = data["tipo_bus"];
-        this.tiempo_viaje = data["tiempo_viaje"];
+        this.tiempo_viaje = this.viaje(data["tiempo_viaje"]);
         this.costo = data["costo"];        
-        this.precio = this.calcularPrecio(this.tipo_bus, parseFloat(this.costo));
+        this.precio = this.calcularPrecio(this.tipo_bus, parseFloat(this.costo)).toFixed(2);
 
       },(error) => {
         console.log(error.error);
@@ -63,6 +63,31 @@ export class ResultadoBusquedaBoletoComponent implements OnInit {
   
     console.log("pago: ", pago);
     return pago
+  }
+
+  // 2019-11-13T18:00:00-06:00 /*horarioBD*/
+  horaFecha(horarioBD){
+    var fecha = horarioBD.slice(0,10);
+    var hora  = horarioBD.slice(11,19);
+    var form  = "Fecha: " + fecha + " Hora: " + hora;
+    return form;
+  }
+
+  viaje(tiempo){
+    var tiempos = tiempo.toString();
+    var hrs, min;
+    var rs = "";
+    if ( tiempos > 2 ){
+      hrs = tiempos.slice(0,1);
+      min = tiempos.slice(1,3);
+      rs  = hrs + " hrs " + min + " min de viaje.";
+      console.log(rs);
+    }else{
+      min = tiempos.slice(0,2);
+      rs  = min + " min de viaje.";
+      console.log(rs);
+    }
+    return rs;
   }
 }
 
